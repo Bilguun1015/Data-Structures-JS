2adafe7836b4a0cdb3a5ec5ac066e1f7f14db951
Bilguun <bbg.1015@gmail.com>
2020-09-12 13:57:52 -0500

    implementing Singly Linked List

---
+++ Singly-Linked-Lists/index.js
@@ -0,0 +1,58 @@
+// piece of data - val
+// reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    // removing from the end of the list
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push('hello');
+list.push('hi');
+list.push('hojo');
+
+list.pop();
+list.pop();
+console.log(list);
