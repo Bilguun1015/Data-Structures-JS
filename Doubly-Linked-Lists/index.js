4911eb55bb2bc16b42b4430d22c6e5f30394bc25
Bilguun Nomch <bbg.1015@gmail.com>
2020-09-21 17:50:05 -0500

    added push and pop method to dll

--- Doubly-Linked-Lists/index.js
+++ Doubly-Linked-Lists/index.js
@@ -12,4 +12,43 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let removed = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removed.prev;
+      this.tail.next = null;
+      removed.prev = null;
+    }
+    this.length--;
+    return removed;
+  }
 }
+
+let list = new DoublyLinkedList();
+
+list.push(100);
+list.push(99);
+list.push(98);
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+// console.log(list);
