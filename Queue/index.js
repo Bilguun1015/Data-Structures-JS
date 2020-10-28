c2a6c2c65b897548a00e5520795b7178c7adaca8
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-28 18:26:10 -0500

    making custom queue

--- Queue/index.js
+++ Queue/index.js
@@ -1,1 +1,28 @@
 // FIFO FIrst In First Out
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    let newItem = new Node(val);
+    if (this.first === null) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.tail = newItem;
+    }
+    this.size++;
+  }
+
+  dequeue() {}
+}
