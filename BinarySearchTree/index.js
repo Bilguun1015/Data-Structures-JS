ee6de7f66d6e7f7ac3440579a7fb8ef88353b507
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-29 22:52:45 -0500

    implemented BST insert method

--- BinarySearchTree/index.js
+++ BinarySearchTree/index.js
@@ -10,7 +10,36 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+
+    while (true) {
+      if (val === current.value) return undefined;
+      if (val < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
-tree.root = new Node(10);
+tree.insert(10);
+tree.insert(9);
+console.log(tree.insert(9));
+console.log(tree);
