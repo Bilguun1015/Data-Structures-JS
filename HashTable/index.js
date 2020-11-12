d566d5f22339bec3abd783064150c0a4cd579fc5
Bilguun Nomch <bbg.1015@gmail.com>
2020-11-12 15:23:21 -0600

    added some improvement to the hash function

--- HashTable/index.js
+++ HashTable/index.js
@@ -1,8 +1,12 @@
 const hash = (key, arrayLen) => {
   let total = 0;
-  for (let ch of key) {
-    let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let ch = key[i];
+    let value = ch.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
   }
   return total;
 };
+
+console.log(hash('hello', 13));
