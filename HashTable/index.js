46ba2f606b3a3f95fa5f2338ef966362d3ba5193
Bilguun Nomch <bbg.1015@gmail.com>
2020-11-13 10:35:33 -0600

    implemented hashtable class

--- HashTable/index.js
+++ HashTable/index.js
@@ -1,12 +1,16 @@
-const hash = (key, arrayLen) => {
-  let total = 0;
-  let WEIRD_PRIME = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let ch = key[i];
-    let value = ch.charCodeAt(0) - 96;
-    total = (total * WEIRD_PRIME + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
-};
 
-console.log(hash('hello', 13));
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let ch = key[i];
+      let value = ch.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
