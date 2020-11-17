64d63c6d19d2a2ab5304ac7464b4efcc3e032ae0
Bilguun Nomch <bbg.1015@gmail.com>
2020-11-16 20:44:29 -0600

    added simple removeEdge method withouth edge cases

--- Graph/index.js
+++ Graph/index.js
@@ -6,7 +6,24 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
 }
 
 const g = new Graph();
 g.addVertex('Tokyo');
+g.addVertex('Mongolia');
+g.addVertex('Korea');
+g.addEdge('Tokyo', 'Mongolia');
+g.addEdge('Tokyo', 'Korea');
+console.log(g.adjacencyList);
+g.removeEdge('Korea', 'Tokyo');
+console.log(g.adjacencyList);
