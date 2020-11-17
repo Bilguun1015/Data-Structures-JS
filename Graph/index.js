25609b992aa9b0a6e20f43abfa97cf51466e0b70
Bilguun Nomch <bbg.1015@gmail.com>
2020-11-16 20:31:48 -0600

    simple addVertex method

--- Graph/index.js
+++ Graph/index.js
@@ -2,4 +2,11 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
 }
+
+const g = new Graph();
+g.addVertex('Tokyo');
