f7c335881a1502a3acdda8d9894c36176d5f31b4
Bilguun Nomch <bbg.1015@gmail.com>
2020-11-25 12:30:11 -0600

    finished fibonacci seq

--- Fib/index.js
+++ Fib/index.js
@@ -5,12 +5,25 @@
 //   memo[n] = res;
 //   return res;
 // }
-
-function fib(n, memo = [undefined, 1, 1]) {
+// top down approach using memoization
+function fib_memo(n, memo = [undefined, 1, 1]) {
   if (memo[n] !== undefined) return memo[n];
-  let res = fib(n - 1, memo) + fib(n - 2, memo);
+  let res = fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
   memo[n] = res;
   return res;
 }
 
-console.log(fib(100));
+console.log(fib_memo(100));
+
+// bottom up approach -- tabulated fib
+function fib_table(n) {
+  if (n <= 2) return 1;
+
+  const fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
+
+console.log(fib_table(100));
